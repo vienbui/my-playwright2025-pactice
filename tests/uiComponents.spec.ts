aa9ee68af84ca31db1ded8f100d3d12fb5a57364
vbui <vbui@axon.com>
2025-11-13 15:17:12 +0700

    add lesson 63

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -1,5 +1,6 @@
 import {test, expect} from '@playwright/test';
 import { Location } from '@angular/common';
+// lesson 63 Retries
 
 test.beforeEach(async ({page}) => {
     await page.goto('http://localhost:4200/')
